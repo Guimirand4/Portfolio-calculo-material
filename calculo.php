e9e9aa180783165c892e0a2ff5a978097923423d
Guilherme Miranda <112579139+Guimirand4@users.noreply.github.com>
2024-12-18 02:13:06 +0000

    feat: criar tabela de calculo

--- calculo.php
+++ calculo.php
@@ -1,24 +1,86 @@
 <?php
+//Diz ao PHP que vamos trabalhar usando o protocolo JSON para comunicação
 header('Content-Type: application/json');
+
+//Verificamos se a requisição é do tipo POST,
+//Caso não seja, retornamos uma mensagem de erro
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    //filtramos os dados da requisição e 
+    //atribuímos à uma variável chamada $dados
     $dados = json_decode(file_get_contents("php://input"));
 
-    $areaComodo = $dados -> comodoLargura * $dados -> comodoComprimento;
-    $areaPiso = $dados -> pisoLargura * $dados -> pisoComprimento;
-    
-    $quantidadePiso = $areaComodo / $areaPiso;
+    $feedback = [];
 
-    $margem = $quantidadePiso * ($dados -> margem /100);
+    if(empty($dados->nomeComodo)){
+        $feedback[] = [
+            "idCampo" => "nomeComodo",
+            "mensagem" => "O nome do comôdo/calculo é obrigatório"
+        ] ;
+    }
+
+    if($dados->comodoLargura <= 0){
+        $feedback[] = [
+            "idCampo" => "comodo-largura",
+            "mensagem" => "O valor da largura do comodo deve ser maior que zero"
+        ] ;
+    }
+
+    if($dados->comodoComprimento <= 0){
+        $feedback[] = [
+            "idCampo" => "comodo-comprimento",
+            "mensagem" => "O valor do comprimento do comodo deve ser maior que zero"
+        ];
+    }
+
+    if($dados->pisoLargura <= 0){
+        $feedback[] = [
+            "idCampo" => "piso-largura",
+            "mensagem" => "O valor da largura do piso deve ser maior que zero"
+        ];
+    }
+
+    if($dados->pisoComprimento <= 0){
+        $feedback[] = [
+            "idCampo" => "piso-comprimento",
+            "mensagem" => "O valor do comprimento do piso deve ser maior que zero"
+        ];
+    }
+
+    if($dados->margem <= 0){
+        $feedback[] = [
+            "idCampo" => "margem",
+            "mensagem" => "O valor da margem deve ser maior que zero"
+        ];
+    }
+   
+    if(!empty($feedback)){
+        echo json_encode(["erro" => $feedback]);
+        exit;
+    }
+    $areaComodo = $dados->comodoLargura * $dados->comodoComprimento;
+    $areaPiso = $dados->pisoLargura * $dados->pisoComprimento;
+
+    $quantidadePiso = $areaComodo / $areaPiso;
+    //sleep(10);
+
+    //10% = 0.1
+    //100m * 0.1 = 10m
+    $margem = $quantidadePiso * ($dados->margem / 100);
 
     $quantidadeTotal = $quantidadePiso + $margem;
+
     echo json_encode([
-        "areacomodo" => $areaComodo,
+        "nomeCliente" => $dados->nomeCliente,
+        "nomeComodo" => $dados->nomeComodo,
+        "areaComodo" => $areaComodo,
         "areaPiso" => $areaPiso,
         "quantidade" => $quantidadePiso,
         "quantidadeMargem" => $margem,
         "quantidadeTotal" => $quantidadeTotal
     ]);
-
-}else{
-    echo json_encode(['erro' => 'Método não suportado. Use o POST']);
+    
+} else {
+    echo json_encode(['erro' => 'Método não suportado. USe o POST']);
 }
+?>
